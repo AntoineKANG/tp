bcda133330869b1e028c85a202c8d007c1bdb7a0
AntoineKANG <kzd2001@gmail.com>
2023-05-10 16:08:49 +0200

    TP4 changed

--- data-engineering/tp-docker-kafka/send-messages-to-kafka.sh
+++ data-engineering/tp-docker-kafka/send-messages-to-kafka.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+select item in *; do
+
+done#!/bin/bash
 set -e
 #### Description: Init kafka with messages
 
 # Init values
-container_name='tp-docker-kafka_kafka1_1'
+container_name='tp-docker-kafka-kafka1-1'
 destination='/home/appuser/'
 topic='lyrics'
 file_name='lyrics.txt'
@@ -18,4 +20,4 @@ echo -e "Command Used :"
 echo -e "docker exec $container_name /bin/sh -c  'kafka-console-producer --broker-list localhost:9092 --topic $topic < $destination$file_name'"
 docker exec $container_name /bin/sh -c 'kafka-console-producer --broker-list '$kafka_server' --topic '$topic' < '$destination$file_name
 
-echo -e "Got an error ? Retry it :)"
+echo -e " Got an error ? Retry it :) "
